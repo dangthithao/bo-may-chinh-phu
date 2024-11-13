b384cb95be70f090727409fed8fb546c3a4eb173
Đặng Thị Thảo <160565233+dangthithao@users.noreply.github.com>
2024-11-13 11:57:42 +0700

    Update info

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -1,54 +1,58 @@
 import infoData from "../../../public/data.json";
-interface InfoProps{
-  nhomCondition:String
+
+interface InfoProps {
+  nhomCondition: String;
 }
-const Info= ({ nhomCondition} :InfoProps) => {
+
+const Info = ({ nhomCondition }: InfoProps) => {
+  // Lọc danh sách theo nhomCondition
+  const filteinfoData = infoData.filter((item) => item.nhom === nhomCondition);
+
   return (
     <div>
-      {infoData.length > 0 && (  
-        <h2 className="text-center text-uppercase heading text-white">  
-          {infoData[0].nhom}  
-        </h2>  
-      )}  
-    <div className=" row justify-content-around ">
-      {infoData
-        .filter((item) => item.nhom === nhomCondition)
-        .map((item) => (
-          <div className="col-md-6 my-4 ">
+      {filteinfoData.length > 0 && (
+        // Lấy nhom của phần tử đầu tiên trong mảng đã lọc
+        <h2 className="text-center text-uppercase heading text-white">
+          {filteinfoData[0].nhom}
+        </h2>
+      )}
+      <div className="row justify-content-around">
+        {filteinfoData.map((item) => (
+          <div className="col-md-6 my-4" key={item.hoVaTen}>
             <div className="rounded-3 p-3 info">
-            <div className="row  ">
-              <div className="col">
-                <img
-                  src={item.linkAnh}
-                  alt={item.hoVaTen}
-                  style={{ maxWidth: "100%", height: "auto" }}
-                />
-              </div>
-              <div className="col-7">
-                <p className="text-top">{item.hoVaTen}</p>
-                <p>
-                  <span>Ngày sinh: </span>
-                  {item.ngaySinh}
-                </p>
-                <p>
-                  <span>Chức vụ: </span>
-                  {item.chucVu}
-                </p>
-                <p>
-                  <span>Quê quán: </span>
-                  {item.queQuan}
-                </p>
-                <p>
-                  <span>Trình độ: </span>
-                  {item.trinhDo}
-                </p>
+              <div className="row">
+                <div className="col">
+                  <img
+                    src={item.linkAnh}
+                    alt={item.hoVaTen}
+                    style={{ maxWidth: "100%", height: "auto" }}
+                  />
+                </div>
+                <div className="col-7">
+                  <p className="text-top">{item.hoVaTen}</p>
+                  <p>
+                    <span>Ngày sinh: </span>
+                    {item.ngaySinh}
+                  </p>
+                  <p>
+                    <span>Chức vụ: </span>
+                    {item.chucVu}
+                  </p>
+                  <p>
+                    <span>Quê quán: </span>
+                    {item.queQuan}
+                  </p>
+                  <p>
+                    <span>Trình độ: </span>
+                    {item.trinhDo}
+                  </p>
+                </div>
               </div>
             </div>
-            </div>
-
           </div>
         ))}
-    </div></div>
+      </div>
+    </div>
   );
 };
 
