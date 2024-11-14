1c1ff762567a8c4b5c706573c0f135fbe925335e
Đặng Thị Thảo <160565233+dangthithao@users.noreply.github.com>
2024-11-14 09:29:28 +0700

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build :{
+    rollupOptions: {
+      output: {
+        entryFileNames: 'bo-may-chinh-phu.js',
+        assetFileNames: 'bo-may-chinh-phu.css'
+      },
+    },
+  },
+},  
+)
